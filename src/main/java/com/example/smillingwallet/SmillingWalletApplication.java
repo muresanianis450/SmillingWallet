9381e6850afa0abb3258411cf95f03b3bb9a3ca9
muresanianis450 <ianis.constantin.muresan@stud.ubbcluj.ro>
2026-03-21 18:31:06 +0200

    init project structure

---
+++ src/main/java/com/example/smillingwallet/SmillingWalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.smillingwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmillingWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmillingWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smillingwallet/SmillingWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smillingwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmillingWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
