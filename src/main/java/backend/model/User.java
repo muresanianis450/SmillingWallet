4f3d3f12a0e9c0d2107ec4794da15fddc3709bbc
muresanianis450 <ianis.constantin.muresan@stud.ubbcluj.ro>
2026-05-02 13:34:19 +0300

    Update User Model so it has db config

--- src/main/java/backend/model/User.java
+++ src/main/java/backend/model/User.java
@@ -2,6 +2,7 @@ package backend.model;
 
 import backend.enums.DentalSpecialty;
 import backend.enums.Role;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -9,27 +10,52 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-
+@Entity
+@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
 public class User {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
+
+    @Column(name = "email", nullable = false, unique = true, length = 255)
     private String email;
+
+    @Column(name = "username", nullable = false,length = 100)
     private String username;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "phone", length = 20)
     private String phone;
+
+    @Column(name = "city", length = 100)
     private String city;
+
+    @Column(name = "address", length = 255)
     private String address;
+
+    @Column(name = "rating")
     private Double rating;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "specialty", length = 50)
     private DentalSpecialty specialty; //only for dentists
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role",nullable = false, length = 20)
     private Role role;
+
+    @Column(name = "created_at",nullable = false,updatable = false)
     private LocalDateTime createdAt;
 
-
     public User(String email, String username,String password, String phone, Role role) {
-        this.id = UUID.randomUUID();
         this.email = email;
         this.username = username;
         this.password = password;
@@ -37,7 +63,5 @@ public class User {
         this.role = role;
         this.createdAt = LocalDateTime.now();
     }
-
-
-
+    
 }
